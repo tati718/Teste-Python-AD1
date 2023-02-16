d33d468101aee5ded025b349f0da5decc23bacfe
Tatiane Lopes <53845107+tati718@users.noreply.github.com>
2023-02-15 21:13:02 -0300

    Update segundaQuest.py

--- testefundamentosdeprogramacao/segundaQuest.py
+++ testefundamentosdeprogramacao/segundaQuest.py
@@ -1,22 +1,6 @@
 # Somente o total das vogais
+
 frase = input();
-vogais = 'aeiou'
-frase = frase.lower()
-result = 0
-nvogais = 'lkjhgfdmnbv'
-
-for i in vogais:
-
- if i in vogais:
-    result += frase.count(i)
-
-print('No total há', result, 'vogais na entrada', frase)
-print('Há:', {i: frase.count(i) for i in vogais if i in frase})
-
-else:
-print('Não há vogais na entrada', nvogais)
-
-'''frase = input();
 vogais = 'aeiou'
 frase =frase.lower()
 result = 0
@@ -32,19 +16,11 @@ for i in vogais:
 
 print('No total há' ,result  ,'vogais na entrada' ,frase)
 print( 'Há:',{i:frase.count(i)for i in vogais if i in frase})
-'''
-
-
-
-
-
-
-
 
 #Quantidade de cada vogal de forma individual
 
 
- #return {i: palavra.count(i)for i in vogais if i in palavra}
+ 
  
 
 
